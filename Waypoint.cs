fb8e766ea8662f144627db7ee140012b408e70ef
Leonardo Castro <leocastro0121@icloud.com>
2018-01-24 10:36:45 -0500

    Add files via upload

---
+++ Waypoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField]
+    protected float debugDrawRadius = 1.0f;
+
+    public virtual void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, debugDrawRadius);
+    }
+    
+    // Use this for initialization
+    void Start()
+    {
+        
+
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
